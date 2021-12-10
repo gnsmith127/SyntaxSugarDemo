5e53754e17c52fd9e9a23a1ad3ae6d923874494c
Gianna Smith <gnsmith127@gmail.com>
2021-12-10 16:48:49 -0500

    Fixed confusing code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SyntaxSugarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            
+            if (answer < 9)
+            {
+                Console.WriteLine($"{answer} is less than nine");
+            }
+            else
+            {
+                Console.WriteLine($"{answer} is greater than or equal to nine"); ;
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
